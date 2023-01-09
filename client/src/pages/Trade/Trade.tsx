93792390e84b5a46ed12934327c9643ee5b9faa8
marcelo0410 <cs95401156@gmail.com>
2023-01-10 02:08:08 +1100

    trade function

--- client/src/pages/Trade/Trade.tsx
+++ client/src/pages/Trade/Trade.tsx
@@ -34,31 +34,52 @@ const Trade = () => {
     }
 
     const [coinData, setCoinData] = useState<CoinResult[]>([]);
-    const [selectedCoin, setSelectedCoin] = useState<CoinResult>({});
+    const [userAud, setUserAud] = useState<number>(0);
+    const [convertedCoin, setConvertedCoin] = useState<number>(0);
+    const [selectedCoin, setSelectedCoin] = useState<CoinResult>({} as CoinResult);
 
     useEffect(() => {
-        fetchCoins().then(data => {setCoinData(data.data); console.log(data);});
+        fetchCoins().then(data => {setCoinData(data.data); console.log(data); filterCoin(data.data, 'bitcoin')});
         
     }, [])
 
-    const filterCoin = (data: Array<CoinResult> ) => {
-        data.filter(item => {
-            if(item.id === 'btc') return item;
-        }) 
+    const filterCoin = (data: Array<CoinResult>, targetCoin: string): void => {
+        let selectedC = data.filter(item => {
+            if(item.id === targetCoin) return item
+        })[0];
+        console.log(selectedC);
+        setSelectedCoin(selectedC);
+    }
+
+    // utils?
+    const calculateCoinQuan = (userInputAud: number) => {
+        setUserAud(Number(userInputAud));
+        setConvertedCoin(userAud/selectedCoin.current_price);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('userprice: ', userAud);
+        console.log(', quantity: ', convertedCoin);
+        console.log('coin:', selectedCoin);
     }
 
   return (
     <div style={{width: '50%', justifyContent: 'center'}}>
-        <div className="mb-6">
-            <label htmlFor="username-success" className="block mb-2 text-sm font-medium text-green-700 dark:text-green-500">AUD</label>
-            <input type="text" id="username-success" className="bg-green-50 border border-green-500 text-green-900 placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-green-100 dark:border-green-400" placeholder="0"/>
-            {/* <p className="mt-2 text-sm text-green-600 dark:text-green-500"><span className="font-medium">Alright!</span> Username available!</p> */}
-        </div>
-        <div>
-            <label htmlFor="username-error" className="block mb-2 text-sm font-medium text-red-700 dark:text-red-500">Receive</label>
-            <input type="text" id="username-error" className="bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-red-100 dark:border-red-400" placeholder="0"/>
-            {/* <p className="mt-2 text-sm text-red-600 dark:text-red-500"><span className="font-medium">Oops!</span> Username already taken!</p> */}
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div className="mb-6">
+                <label htmlFor="username-success" className="block mb-2 text-sm font-medium text-green-700 dark:text-green-500">AUD</label>
+                <input type="text" id="username-success" className="bg-green-50 border border-green-500 text-green-900 placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-green-100 dark:border-green-400" value={userAud} onChange={e => calculateCoinQuan(Number(e.target.value))}/>
+                {/* <p className="mt-2 text-sm text-green-600 dark:text-green-500"><span className="font-medium">Alright!</span> Username available!</p> */}
+            </div>
+            <div>
+                <label htmlFor="username-error" className="block mb-2 text-sm font-medium text-red-700 dark:text-red-500">Receive</label>
+                <div id="username-error" className="bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-red-100 dark:border-red-400" >{convertedCoin}</div>
+                {/* <p className="mt-2 text-sm text-red-600 dark:text-red-500"><span className="font-medium">Oops!</span> Username already taken!</p> */}
+                <div>{selectedCoin.id} Current price: {selectedCoin.current_price}</div>
+            </div>
+            <button type='submit' >Buy</button>
+        </form>
     </div>
   )
 }
